a6264d0bc083c0ad2254a78e081333c105c266b7
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-02-26 08:22:53 -0300

    Formato XML e erros concentrados em classes.

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,19 +1,80 @@
 const roteador = require("express").Router();
 const TabelaFornecedor = require("./TabelaFornecedor");
 const Fornecedor = require("./Fornecedor");
+const { SerializadorFornecedor } = require("./../../serializador/Serializador");
+// const Serializador = require("./../../serializador/Serializador");
 
 roteador.get("/", async (req, res) => {
   const resultados = await TabelaFornecedor.listar();
-  res.json({
-    resultados,
-  });
+  res.status(200);
+  const serializador = new SerializadorFornecedor(
+    res.getHeader("Content-Type")
+  );
+
+  res.send(serializador.serializar(resultados));
 });
 
-roteador.post("/", async (req, res) => {
-  const dadosRecebidos = req.body;
-  const fornecedor = new Fornecedor(dadosRecebidos);
-  await fornecedor.criar();
-  res.send(JSON.stringify(fornecedor));
+roteador.post("/", async (req, res, next) => {
+  try {
+    const dadosRecebidos = req.body;
+    const fornecedor = new Fornecedor(dadosRecebidos);
+    await fornecedor.criar();
+    res.status(201);
+
+    const serializador = new SerializadorFornecedor(
+      res.getHeader("Content-Type")
+    );
+
+    res.send(serializador.serializar(fornecedor));
+  } catch (erro) {
+    next(erro);
+  }
 });
 
+roteador.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const fornecedor = new Fornecedor({ id: id });
+    res.status(200);
+
+    await fornecedor.carregar();
+
+    const serializador = new SerializadorFornecedor(
+      res.getHeader("Content-Type"),
+      ["email", "dataCriacao", "dataAtualizacao", "versao"]
+    );
+
+    res.send(serializador.serializar(fornecedor));
+  } catch (erro) {
+    next(erro);
+  }
+});
+
+roteador.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const dadosRecebidos = req.body;
+    const dados = Object.assign({}, dadosRecebidos, { id: id });
+    const fornecedor = new Fornecedor(dados);
+
+    await fornecedor.atualizar();
+    res.status(204);
+    res.end();
+  } catch (erro) {
+    next(erro);
+  }
+});
+
+roteador.delete("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const fornecedor = new Fornecedor({ id: id });
+    await fornecedor.carregar();
+    await fornecedor.remover();
+    res.status(204);
+    res.end();
+  } catch (erro) {
+    next(erro);
+  }
+});
 module.exports = roteador;
--- config/app.js
+++ config/app.js
@@ -1,9 +1,48 @@
 const express = require("express");
 const app = express();
 const roteadorFornecedores = require("./../api/rotas/fornecedores/index");
+const NaoEncontrado = require("./../api/erros/NaoEncontrado");
+const CampoInvalido = require("./../api/erros/CampoInvalido");
+const DadosNaoFornecidos = require("../api/erros/DadosNaoFornecidos");
+const ValorNaoSuportado = require("./../api/erros/ValorNaoSuportado");
+const { formatosAceitos } = require("./../api/serializador/Serializador");
+const { SerializadorErro } = require("./../api/serializador/Serializador");
 
 app.use(express.json());
+app.use((req, res, next) => {
+  const formatoRequisitado = req.header("Accept");
+
+  if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+    res.status(406);
+    res.end();
+  }
+
+  res.setHeader("Content-Type", formatoRequisitado);
+  next();
+});
 
 app.use("/api/fornecedores", roteadorFornecedores);
+app.use((erro, req, res, proximo) => {
+  let status = 500;
+  if (erro instanceof NaoEncontrado) {
+    status = 404;
+  } else if (
+    erro instanceof CampoInvalido ||
+    erro instanceof DadosNaoFornecidos
+  ) {
+    status = 400;
+  } else if (erro instanceof ValorNaoSuportado) {
+    status = 406;
+  }
+
+  const serializador = new SerializadorErro(res.getHeader("Content-Type"));
+  res.status(status);
+  res.send(
+    serializador.serializar({
+      mensagem: erro.message,
+      id: erro.idErro,
+    })
+  );
+});
 
 module.exports = app;
